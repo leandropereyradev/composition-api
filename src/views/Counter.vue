069e45ea2fc7ecf279bce2e09114785a8ba99057
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-21 13:58:49 +0200

    Basic Setap()

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,9 +1,26 @@
 <template>
   <h1>Counter View</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+ 1</button>
+  <button @click="decrease">- 1</button>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
+  name: 'Counter',
+  setup() {
+    const counter = ref(5)
+
+    return {
+      counter,
+
+      decrease: () => counter.value--,
+      increase: () => counter.value++,
+    }
+  }
 
 }
 </script>
